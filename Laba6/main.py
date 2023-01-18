e59eaedbc6978da82c9e2cc50925f16e71b162a1
Gleb-Matveev <61318934+Gleb-Matveev@users.noreply.github.com>
2023-01-18 22:16:23 +0300

    Add files via upload

--- Laba6/main.py
+++ Laba6/main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def calc(nm1, nm2, op):
+    if op == "+":
+        res = nm1 + nm2
+        return res
+    elif op == "-":
+        res = nm1 - nm2
+        return res
+    elif op == "*":
+        res = nm1 * nm2
+        return res
+    elif op == "/":
+        if nm2 == 0:
+            res = "undefined"
+            return res
+        else:
+            res = nm1 / nm2
+            return res
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    opera = input("Enter an operation: ")
+    num1 = int(input("Enter a first num: "))
+    num2 = int(input("Enter a second num: "))
+    #print(num1, num2, opera)
+    print(calc(num1, num2, opera))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
