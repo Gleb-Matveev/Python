ec2f54e26ce069c69d904df9863e3d9fb8cc1591
Gleb-Matveev <61318934+Gleb-Matveev@users.noreply.github.com>
2023-01-18 23:35:04 +0300

    Update main.py

--- Laba6/main.py
+++ Laba6/main.py
@@ -19,9 +19,9 @@ def calc(nm1, nm2, op):
 
 
 if __name__ == '__main__':
-    opera = input("Enter an operation: ")
-    num1 = int(input("Enter a first num: "))
-    num2 = int(input("Enter a second num: "))
-    print(calc(num1, num2, opera))
+    #opera = input("Enter an operation: ")
+    #num1 = int(input("Enter a first num: "))
+    #num2 = int(input("Enter a second num: "))
+    #print(calc(num1, num2, opera))
 
 
